8eb9c517b2cad9492d5febe8327dd63e1922b8cf
Chainz <61576690+Chainz7@users.noreply.github.com>
2023-01-22 15:35:35 +0700

    approver-approve-done

--- resources/views/approver/pages/approve.blade.php
+++ resources/views/approver/pages/approve.blade.php
@@ -1,7 +1,7 @@
 @include('approver.head')
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pages /</span> Monitoring</h4>
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pages /</span> Approve</h4>
         <div class="card-group mb-5">
 
             <div class="card">
